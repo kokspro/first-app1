26b9ca2f0fd16feecbddf19e618d8129422702bf
kokspro <koks.programming@gmail.com>
2023-02-28 16:00:41 -0600

    Modules explination

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-function sayHello(name) {
-    console.log('Hello ' + name);
-}
+// function sayHello(name) {
+//     console.log('Hello ' + name);  //console is part of the global scope
+// }
 
-sayHello('Willy');
+// sayHello('Willy');
+
+//console.log()  //global scope via the window object
+//setTimeout() 
+//clearTimeout()
+//setInterval()
+//clearInterval
+//window object is in the browser only, but is not in node
+//in node you use global. instead of window.
+
+
+//global object
+// var message = '';    //this will come out undefined 
+// console.log(global.message);
+
+//Modules in Node
+//Every node application has atleast one file/module   
+ console.log(module);
