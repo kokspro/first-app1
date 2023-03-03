c67aeedb948d403ffca9748732e73098ecc6be94
kokspro <koks.programming@gmail.com>
2023-03-03 17:27:38 -0600

    Express Install

--- app.js
+++ app.js
@@ -137,8 +137,23 @@
 
 // console.log('Listening on port 4000...');
 
-//NPM
+//Introducing Express -- Which is a framework
 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3, 4, 5]));
+        res.end();
+    }
+});
+
+server.listen(3000);
+console.log('Listening on port 3000. . .');
 
 
 
