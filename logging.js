75d7f61716ebeae585a04fd954b8b6a9865935f5
Kokspro <thatdadgaming@gmail.com>
2024-11-25 09:37:44 -0600

    Showing professor JS

---
+++ logging.js
@@ -0,0 +1,6 @@
+let variable = 5;
+console.log(variable);
+variable = 'Now I am a string';
+console.log(variable);
+variable = [5, 6, 'String', [5, 'Strings in arrays'], 5, 7];
+console.log(variable);
